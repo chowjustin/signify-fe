3375475244862a29c609482305a9cca00f43980f
Justin Chow <justin.chow05@gmail.com>
2024-10-20 14:35:42 +0700

    feat: add animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+import * as React from "react";
+
 import NextImage from "@/components/NextImage";
 import Typography from "@/components/Typography";
 
 export default function Home() {
+  const [text, setText] = React.useState("Signify");
+
   return (
     <main className="bg-[url('/images/bg.png')] w-screen h-screen flex flex-col items-center gap-2 md:gap-6 justify-center">
       <NextImage
@@ -10,14 +15,18 @@ export default function Home() {
         height={2000}
         alt="Signify"
         className="max-w-[50%] md:max-w-[20%]"
+        onMouseEnter={() => setText("Coming soon!")}
+        onMouseLeave={() => setText("Signify")}
       />
-      <Typography
-        variant="h1"
-        weight="bold"
-        className="text-white max-sm:text-[40px]"
-      >
-        Signify
-      </Typography>
+      <div className="w-max">
+        <Typography
+          variant="h1"
+          weight="bold"
+          className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl text-white font-bold max-sm:text-[40px]"
+        >
+          {text}
+        </Typography>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,28 @@ const config: Config = {
       fontFamily: {
         inter: ["(var(--font-inter))"],
       },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden",
+          },
+          "100%": {
+            width: "100%",
+          },
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent",
+          },
+          "100%": {
+            borderColor: "white",
+          },
+        },
+      },
+      animation: {
+        typing: "typing 2s steps(20) infinite alternate, blink .7s infinite",
+      },
     },
   },
   plugins: [],
