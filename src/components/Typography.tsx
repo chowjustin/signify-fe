ad9a930f08dcb6ea69f705f68410105ad0323758
amtsal <amtsal.99@gmail.com>
2024-11-05 10:56:43 +0700

    feat: add profile page

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -61,6 +61,7 @@ export default function Typography<T extends React.ElementType>({
               weight === "regular" && "font-normal",
               weight === "medium" && "font-medium",
               weight === "bold" && "font-bold",
+              weight === "black" && "font-black",
             ],
           ],
         ],
