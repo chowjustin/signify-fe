7b80512196c0af7232c3baebe0e200a54d767781
Justin Chow <justin.chow05@gmail.com>
2024-11-09 02:06:12 +0700

    feat: add detail page and integrate inbox page

---
+++ src/app/dashboard/[id]/page.tsx
@@ -0,0 +1,182 @@
+"use client";
+import {
+  useForm,
+  FormProvider,
+  SubmitHandler,
+  useWatch,
+} from "react-hook-form";
+import SelectableInput from "@/components/form/SelectableInput";
+import { usePathname, useRouter } from "next/navigation";
+import { AxiosError, AxiosResponse } from "axios";
+import { useMutation } from "@tanstack/react-query";
+import { ApiError } from "@/types/api";
+import api from "@/lib/api";
+import toast from "react-hot-toast";
+import Typography from "@/components/Typography";
+import Input from "@/components/form/Input";
+import UploadFile from "@/components/form/UploadFile";
+import Button from "@/components/buttons/Button";
+import LabelText from "@/components/form/LabelText";
+import NextImage from "@/components/NextImage";
+import BreadCrumbs from "@/components/BreadCrumbs";
+import TextArea from "@/components/form/TextArea";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import { Worker, Viewer, SpecialZoomLevel } from "@react-pdf-viewer/core";
+import { RenderPageProps } from "@react-pdf-viewer/core";
+import { useState } from "react";
+import { AcceptModal } from "./modal/acceptModal";
+import { RejectModal } from "./modal/rejectModal";
+
+type SignUpRequest = {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  file: FileList | null;
+};
+
+export default function TambahAjuan() {
+  const methods = useForm<SignUpRequest>({ mode: "onChange" });
+  const { handleSubmit, control } = methods;
+
+  const path = usePathname();
+  const pathId = path.split("/").pop();
+
+  const breadCrumbs = [
+    { href: "/dashboard", Title: "Dashboard" },
+    { href: `/dashboard/${pathId}`, Title: "Detail" },
+  ];
+
+  const router = useRouter();
+  // Watch for file upload changes
+  const file = useWatch({ control, name: "file" });
+  const [coordinates, setCoordinates] = useState<{
+    x: number;
+    y: number;
+  } | null>(null);
+  const handlePageClick = (e: React.MouseEvent, page: HTMLElement) => {
+    if (page instanceof HTMLElement) {
+      const rect = page.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      setCoordinates({ x, y });
+    }
+  };
+  const renderPage = (props: RenderPageProps) => {
+    return (
+      <div
+        style={{
+          position: "relative",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
+          overflow: "hidden",
+          cursor: "crosshair",
+          zIndex: 100,
+        }}
+        onClick={(e) => handlePageClick(e, e.currentTarget)}
+      >
+        {props.canvasLayer.children}
+      </div>
+    );
+  };
+  const { mutate: SignUpMutation, isPending } = useMutation<
+    AxiosResponse,
+    AxiosError<ApiError>,
+    SignUpRequest
+  >({
+    mutationFn: async (data: SignUpRequest) => {
+      const formData = new FormData();
+      formData.append("name", data.name);
+      formData.append("username", data.username);
+      formData.append("email", data.email);
+      formData.append("password", data.password);
+      if (data.file && data.file[0]) {
+        formData.append("file", data.file[0]);
+      }
+      return await api.post("/users/signup", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+    },
+    onSuccess: () => {
+      toast.success("Berhasil melakukan registrasi!");
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+  const onSubmit: SubmitHandler<SignUpRequest> = (data) => {
+    SignUpMutation(data);
+  };
+  const fileUrl = file?.[0] ? URL.createObjectURL(file[0]) : null;
+  return (
+    <section className="p-6 pb-12">
+      <div className="relative w-full h-full rounded-[15px] min-h-[64px] overflow-hidden">
+        <NextImage
+          src="/bgbreadcrumb.png"
+          width={1920}
+          height={1080}
+          alt="Background"
+          className="w-full h-full -z-10 min-h-[64px]"
+          imgClassName="w-full h-[64px] md:h-[80px]"
+        />
+        <div className="absolute z-10 top-4 left-6 max-md:top-2 max-md:left-3 max-md:mb-0">
+          <BreadCrumbs breadcrumbs={breadCrumbs} />
+          <span className="font-semibold text-white max-md:-pt-1">
+            Detail Ajuan Tanda Tangan
+          </span>
+        </div>
+      </div>
+      <div className="w-full pt-[30px]">
+        <h1 className="font-bold text-xl">Judul</h1>
+        <p className="text-sm mt-3">Sender</p>
+        <h6 className="text-md mt-[30px]">cover letter</h6>
+        {/* File Preview */}
+        <LabelText labelTextClasname="mt-8 mb-4">File Preview</LabelText>
+        <div className="lg:w-[75%] w-full 2xl:w-[50%] bg-gray-100 h-[80dvh] p-4 border-3 border-primary border-dashed rounded-lg overflow-auto">
+          {fileUrl ? (
+            <Worker
+              workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}
+            >
+              <Viewer
+                fileUrl={fileUrl}
+                defaultScale={SpecialZoomLevel.PageFit}
+                renderPage={renderPage}
+              />
+            </Worker>
+          ) : (
+            <p className="text-center text-gray-500">File not uploaded</p>
+          )}
+        </div>
+      </div>
+      <div className="lg:w-[75%] w-full 2xl:w-[50%] flex justify-center gap-4 max-md:gap-2 mt-6">
+        <RejectModal path="/dashboard">
+          {({ openModal }) => (
+            <Button
+              variant="red"
+              size="base"
+              className="min-h-8 max-w-24 px-9 py-0.5"
+              onClick={openModal}
+            >
+              Tolak
+            </Button>
+          )}
+        </RejectModal>
+        <AcceptModal>
+          {({ openModal }) => (
+            <Button
+              variant="primary"
+              size="base"
+              className="min-h-8 max-w-24 px-9 py-0.5"
+              onClick={openModal}
+            >
+              Terima
+            </Button>
+          )}
+        </AcceptModal>
+      </div>
+    </section>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,120 +8,131 @@ import NextImage from "@/components/NextImage";
 import Link from "next/link";
 import { FiMail } from "react-icons/fi";
 import { FaRegPaperPlane } from "react-icons/fa";
+import { useQuery } from "@tanstack/react-query";
+import api from "@/lib/api";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 
-type InboxItem = {
-  sender_id: number;
-  sender_name: string;
-  sender_username: string;
-  sender_topic: string;
-  sender_coverletter: string;
-  sender_datetime: string;
+type InboxData = {
+  ID: string;
+  Name: string;
+  Username: string;
+  Email: string;
+  Topic: string;
+  CoverLetter: string;
+  Status: string;
+  CreatedAt: string;
 };
-
-type SentItem = {
-  receiver_id: number;
-  receiver_name: string;
-  receiver_username: string;
-  receiver_topic: string;
-  receiver_coverletter: string;
-  receiver_datetime: string;
+type SentData = {
+  S_ID: string;
+  S_Name: string;
+  S_Username: string;
+  S_Email: string;
+  S_Topic: string;
+  S_CoverLetter: string;
+  S_Status: string;
+  S_CreatedAt: string;
 };
-
-const inbox: InboxItem[] = [
-  {
-    sender_id: 1,
-    sender_name: "Nama 1",
-    sender_username: "username",
-    sender_topic: "Permohonan ajuan ttd Genetic Algorithm",
-    sender_coverletter: "Bacot disini kalau ada cover letter",
-    sender_datetime: "6 Nov 2024",
-  },
-  {
-    sender_id: 2,
-    sender_name: "Nama 2",
-    sender_username: "username",
-    sender_topic: "Permohonan ajuan ttd Genetic Algorithm",
-    sender_coverletter: "Bacot disini kalau ada cover letter",
-    sender_datetime: "6 Nov 2024",
-  },
-  {
-    sender_id: 3,
-    sender_name: "Nama 3",
-    sender_username: "username",
-    sender_topic: "Permohonan ajuan ttd Genetic Algorithm",
-    sender_coverletter: "Bacot disini kalau ada cover letter",
-    sender_datetime: "6 Nov 2024",
-  },
-  {
-    sender_id: 4,
-    sender_name: "Nama 4",
-    sender_username: "username",
-    sender_topic: "Permohonan ajuan ttd Genetic Algorithm",
-    sender_coverletter: "Bacot disini kalau ada cover letter",
-    sender_datetime: "6 Nov 2024",
-  },
-  {
-    sender_id: 5,
-    sender_name: "Nama 5",
-    sender_username: "username",
-    sender_topic: "Permohonan ajuan ttd Genetic Algorithm",
-    sender_coverletter: "Bacot disini kalau ada cover letter",
-    sender_datetime: "6 Nov 2024",
-  },
-  {
-    sender_id: 6,
-    sender_name: "Nama 6",
-    sender_username: "username",
-    sender_topic: "Permohonan ajuan ttd Genetic Algorithm",
-    sender_coverletter: "Bacot disini kalau ada cover letter",
-    sender_datetime: "6 Nov 2024",
-  },
-  {
-    sender_id: 7,
-    sender_name: "Nama 7",
-    sender_username: "username",
-    sender_topic: "Permohonan ajuan ttd Genetic Algorithm",
-    sender_coverletter: "Bacot disini kalau ada cover letter",
-    sender_datetime: "6 Nov 2024",
-  },
-];
-
-const sent: SentItem[] = [
-  {
-    receiver_id: 1,
-    receiver_name: "Nama 1",
-    receiver_username: "username",
-    receiver_topic: "Permohonan ajuan ttd Genetic Algorithm",
-    receiver_coverletter: "Bacot disini kalau ada cover letter",
-    receiver_datetime: "6 Nov 2024",
-  },
-  {
-    receiver_id: 2,
-    receiver_name: "Nama 2",
-    receiver_username: "username",
-    receiver_topic: "Permohonan ajuan ttd Genetic Algorithm",
-    receiver_coverletter: "Bacot disini kalau ada cover letter",
-    receiver_datetime: "6 Nov 2024",
-  },
-  {
-    receiver_id: 3,
-    receiver_name: "Nama 3",
-    receiver_username: "username",
-    receiver_topic: "Permohonan ajuan ttd Genetic Algorithm",
-    receiver_coverletter: "Bacot disini kalau ada cover letter",
-    receiver_datetime: "6 Nov 2024",
-  },
-];
 
 const breadCrumbs = [{ href: "/dashboard", Title: "Dashboard" }];
 
-function isInboxItem(item: InboxItem | SentItem): item is InboxItem {
-  return (item as InboxItem).sender_name !== undefined;
+function isInboxItem(item: InboxData | SentData): item is InboxData {
+  return (item as InboxData).Name !== undefined;
 }
 
 export default withAuth(Dashboard, "user");
 function Dashboard() {
   const [selectedView, setSelectedView] = useState("inbox");
+
+  const [currentInboxPage, setCurrentInboxPage] = useState(0);
+  const [currentSentPage, setCurrentSentPage] = useState(0);
+
+  const { data: inbox } = useQuery({
+    queryKey: ["inbox", currentInboxPage],
+    queryFn: async () => {
+      const response = await api.get(`/sign/inbox?page=${currentInboxPage}`);
+      return response.data;
+    },
+  });
+
+  const { data: sent } = useQuery({
+    queryKey: ["sent", currentSentPage],
+    queryFn: async () => {
+      const response = await api.get(`/sign/sent?page=${currentSentPage}`);
+      return response.data;
+    },
+  });
+
+  const inboxTotalPages = inbox?.total_pages - 1;
+  const sentTotalPages = sent?.total_pages - 1;
+
+  const handleInboxNextPage = () => {
+    if (currentInboxPage < inboxTotalPages) {
+      setCurrentInboxPage((prev) => prev + 1);
+    }
+  };
+
+  const handleInboxPrevPage = () => {
+    if (currentInboxPage > 0) {
+      setCurrentInboxPage((prev) => prev - 1);
+    }
+  };
+
+  const handleSentNextPage = () => {
+    if (currentSentPage < sentTotalPages) {
+      setCurrentSentPage((prev) => prev + 1);
+    }
+  };
+
+  const handleSentPrevPage = () => {
+    if (currentSentPage > 0) {
+      setCurrentSentPage((prev) => prev - 1);
+    }
+  };
+
+  function parseDate(datetimeString: string) {
+    const dateObject = new Date(datetimeString);
+
+    if (isNaN(dateObject.getTime())) {
+      console.error("Invalid date:", datetimeString);
+      return "";
+    }
+
+    const year = dateObject.getFullYear();
+    const monthIndex = dateObject.getMonth();
+    const day = String(dateObject.getDate()).padStart(2, "0");
+
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    const month = monthNames[monthIndex];
+
+    const hours = String(dateObject.getHours()).padStart(2, "0");
+    const minutes = String(dateObject.getMinutes()).padStart(2, "0");
+    const time = `${hours}:${minutes}`;
+
+    return (
+      <>
+        <div>
+          <div>
+            {day} {month} {year}
+          </div>
+          {time}
+        </div>
+      </>
+    );
+  }
 
   return (
     <section className="p-6 max-md:p-3">
@@ -164,7 +175,56 @@ function Dashboard() {
             <FaRegPaperPlane className="text-xl" />
           </Button>
         </div>
-        <div className="text-sm max-md:text-xs">Page 1 dari 10 </div>
+
+        {selectedView === "inbox" ? (
+          <>
+            {inboxTotalPages > 0 && (
+              <div className="flex items-center justify-between mt-4">
+                <span className="text-sm max-md:text-xs">
+                  Page {currentInboxPage} dari {inboxTotalPages}
+                </span>
+                <button
+                  onClick={handleInboxPrevPage}
+                  disabled={currentInboxPage === 0}
+                  className=" text-black px-4 py-2 rounded disabled:opacity-50 disabled:cursor-not-allowed"
+                >
+                  <FaAngleLeft />
+                </button>
+                <button
+                  onClick={handleInboxNextPage}
+                  disabled={currentInboxPage === inboxTotalPages}
+                  className=" text-black px-4 py-2 rounded disabled:opacity-50 disabled:cursor-not-allowed"
+                >
+                  <FaAngleRight />
+                </button>
+              </div>
+            )}
+          </>
+        ) : (
+          <>
+            {sentTotalPages > 0 && (
+              <div className="flex items-center justify-between mt-4">
+                <span className="text-sm max-md:text-xs">
+                  Page {currentSentPage} dari {sentTotalPages}
+                </span>
+                <button
+                  onClick={handleSentPrevPage}
+                  disabled={currentSentPage === 0}
+                  className=" text-black px-4 py-2 rounded disabled:opacity-50 disabled:cursor-not-allowed"
+                >
+                  <FaAngleLeft />
+                </button>
+                <button
+                  onClick={handleSentNextPage}
+                  disabled={currentSentPage === sentTotalPages}
+                  className=" text-black px-4 py-2 rounded disabled:opacity-50 disabled:cursor-not-allowed"
+                >
+                  <FaAngleRight />
+                </button>
+              </div>
+            )}
+          </>
+        )}
       </div>
 
       <div className="w-full rounded-lg">
@@ -183,50 +243,56 @@ function Dashboard() {
         </div>
 
         <div>
-          {(selectedView === "inbox" ? inbox : sent).map((item, index) => (
-            <Link
-              href={"/inbox/detail"}
-              key={index}
-              className="w-full grid grid-cols-12 py-3 px-4 border-b-1 gap-x-2 justify-center items-center text-center hover:shadow-md hover:border-1"
-            >
-              <div className="col-span-2 max-md:col-span-12 max-md:flex max-md:items-center max-md:gap-2 text-left">
-                {isInboxItem(item) ? (
-                  <>
-                    <p className="font-bold max-md:text-sm">
-                      {item.sender_name}
+          {(selectedView === "inbox" ? inbox?.data : sent?.data)?.length > 0 ? (
+            (selectedView === "inbox" ? inbox.data : sent.data).map(
+              (item: InboxData | SentData, index: number) => (
+                <Link
+                  href={`/dashboard/${
+                    isInboxItem(item) ? item.ID : item.S_ID
+                  } `}
+                  key={index}
+                  className="w-full grid grid-cols-12 py-3 px-4 border-b-1 gap-x-2 justify-center items-center text-center hover:shadow-md hover:border-1"
+                >
+                  <div className="col-span-2 max-md:col-span-12 max-md:flex max-md:items-center max-md:gap-2 text-left">
+                    {isInboxItem(item) ? (
+                      <>
+                        <p className="font-bold max-md:text-sm">{item.Name}</p>
+                        <p className="text-sm max-md:text-xs text-gray-500">
+                          {item.Username}
+                        </p>
+                      </>
+                    ) : (
+                      <>
+                        <p className="font-bold max-md:text-sm">
+                          {item.S_Name}
+                        </p>
+                        <p className="text-sm max-md:text-xs text-gray-500">
+                          {item.S_Username}
+                        </p>
+                      </>
+                    )}
+                  </div>
+                  <div className="col-span-8 max-md:col-span-10 text-left">
+                    <p className="font-semibold max-md:text-sm">
+                      {isInboxItem(item) ? item.Topic : item.S_Topic}
                     </p>
-                    <p className="text-sm max-md:text-xs text-gray-500">
-                      {item.sender_username}
+                    <p className="text-sm max-md:text-xs text-gray-500 truncate overflow-hidden whitespace-nowrap">
+                      {isInboxItem(item)
+                        ? item.CoverLetter
+                        : item.S_CoverLetter}
                     </p>
-                  </>
-                ) : (
-                  <>
-                    <p className="font-bold max-md:text-sm">
-                      {item.receiver_name}
-                    </p>
-                    <p className="text-sm max-md:text-xs text-gray-500">
-                      {item.receiver_username}
-                    </p>
-                  </>
-                )}
-              </div>
-              <div className="col-span-8 max-md:col-span-10 text-left">
-                <p className="font-semibold max-md:text-sm">
-                  {isInboxItem(item) ? item.sender_topic : item.receiver_topic}
-                </p>
-                <p className="text-sm max-md:text-xs text-gray-500 truncate overflow-hidden whitespace-nowrap">
-                  {isInboxItem(item)
-                    ? item.sender_coverletter
-                    : item.receiver_coverletter}
-                </p>
-              </div>
-              <p className="col-span-2 text-right max-md:text-xs">
-                {isInboxItem(item)
-                  ? item.sender_datetime
-                  : item.receiver_datetime}
-              </p>
-            </Link>
-          ))}
+                  </div>
+                  <p className="col-span-2 text-right max-md:text-xs">
+                    {isInboxItem(item)
+                      ? parseDate(item.CreatedAt)
+                      : parseDate(item.S_CreatedAt)}
+                  </p>
+                </Link>
+              ),
+            )
+          ) : (
+            <p>No items available</p>
+          )}
         </div>
       </div>
     </section>
